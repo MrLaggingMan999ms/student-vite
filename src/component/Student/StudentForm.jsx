9913d57b314e769de75e75f2702630a85009681f
MrL <mrlaggingman999ms@gmail.com>
2024-10-06 15:13:48 +0630

    init

--- src/component/Student/StudentForm.jsx
+++ src/component/Student/StudentForm.jsx
@@ -1,34 +1,76 @@
 import { useState } from "react";
 
 const StudentForm = ({ setStudent }) => {
-    const [name, setName] = useState("");
-    const [avatar, setAvatar] = useState("");
-    const [major, setMajor] = useState("");
+  // const [name, setName] = useState("");
+  // const [avatar, setAvatar] = useState("");
+  // const [major, setMajor] = useState("");
 
-    console.log({name});
-    console.log({avatar});
-    console.log({major});
-
-    const handleSubmit = () =>{
-        e.preventDefault();
-        console.log({name, avatar, major});
-    };
+  // console.log({name});
+  // console.log({avatar});
+  // console.log({major});
+  const [student, setStu] = useState({
+    name:"",
+    avatar:"",
+    major:"major",
+  })
+  const getID = () => {
+    return Math.round(Math.random() * 1000);
+  };
+  const frmReset = () => {
+    // setName("");
+    // setAvatar("");
+    // setMajor("major");
+    setStu({name: "", avatar: "", major:"0"});
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(student);
+    const {name, major, avatar} = student;
+    if (!name || !major || !avatar) {
+      alert("Please fill your data");
+    } else {
+      const newStudent = { sID: getID(), name, avatar, major };
+      setStudent((prev) => [...prev, newStudent]);
+      frmReset();
+    }
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={student.name}
+            onChange={(e) => 
+              setStu(prev=>({...prev, name: e.target.value}))}
+          />
         </div>
         <div>
           <label htmlFor="avatar">Avatar</label>
-          <input type="text" name="avatar" id="avatar" value={avatar} onChange={(e) => setAvatar(e.target.value)} />
+          <input
+            type="text"
+            name="avatar"
+            id="avatar"
+            value={student.avatar}
+            onChange={(e) => 
+              setStu(prev=>({...prev, avatar: e.target.value}))}
+          />
         </div>
         <div>
           <label htmlFor="major">Major</label>
-          <select name="major" id="major" value={major} defaultValue={0} onChange={(e) => setMajor(e.target.value)}>
-            <option value="0">select a major</option>
+          <select
+            name="major"
+            id="major"
+            defaultValue={student.major}
+            value={student.major}
+            onChange={(e) => 
+              setStu(prev=>({...prev, major: e.target.value}))}
+          >
+            <option value="major">select a major</option>
             <option value="math">Math</option>
             <option value="eng">English</option>
             <option value="chem">Chemistry</option>
