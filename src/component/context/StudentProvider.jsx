88fb15cccacadfc83a7d6acbddc154ccb9992cb9
MrL <mrlaggingman999ms@gmail.com>
2024-10-23 11:14:54 +0630

    init

--- src/component/context/StudentProvider.jsx
+++ src/component/context/StudentProvider.jsx
@@ -1,57 +1,51 @@
-// import React, { useState } from 'react'
-import { createContext, useEffect, useReducer } from 'react'
-// import StudentDummy from '../Student/data'
-const initStudent ={
-  data:[],
-  isLoading:false,
-  isError:false
-}
-export const StudentContext = createContext(null)
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useReducer } from "react";
 
-const StudentProvider = ({children}) => {
-  // const [student, setStudents] = useState(StudentDummy)
+export const StudentContext = createContext(null);
 
+const initStudent = {
+  data: [],
+  isLoading: false,
+  isError: false,
+  students: [],
+};
 
-  const studentReducer = (state,action)=>{
-    console.log(action)
-
-    switch (action.type) {
-      case "Fetch":
-        return {...state,data:action.payload, isLoading:false}
-      case "LOADING":
-        return{...state, isLoading:false}
-      default:
-        return {...state};
-    }
+const studentReducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "FETCH":
+      return { ...state, data: action.payload, isLoading: false };
+    case "LOADING":
+      return { ...state, isLoading: true };
+    default:
+      return { ...state };
   }
+};
 
-  const [student, dispatch] = useReducer(studentReducer, initStudent)
-  
-  console.log(students)
+const StudentProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(studentReducer, initStudent);
 
   useEffect(() => {
-    dispatch({type:"LOADING"})
-    fetch ("https://st-api.kaungmyatsoe.dev/api/v1/students",{
+    dispatch({ type: "LOADING" });
+    fetch("https://st-api.kaungmyatsoe.dev/api/v1/students", {
       method: "GET",
-      headers:{
-        keys:"43/UgWoJWW8pXKRmM48xYp8uuIXXLaBM1USAblj50X5GrVUdaluW36lEjoAbylSL6m4g9OXOxb9p7teXUyph5w"
+      headers: {
+        key: "43/UgWoJWW8pXKRmM48xYp8uuIXXLaBM1USAblj50X5GrVUdaluW36lEjoAbylSL6m4g9OXOxb9p7teXUyph5w"
       }
     })
-      .then((res)=>res.json())
-      .then((data)=>{
-        console.log(data)
-        //dispatch 
-        dispatch({type:"FETCH", payload: data})
-      })
-  
-  }, [third])
-  
-  
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // Dispatch
+        dispatch({ type: "FETCH", payload: data });
+      });
+  }, []);
+
   return (
-    <StudentContext.Provider value={{students, dispatch}}>
+    <StudentContext.Provider value={{ state, dispatch }}>
       {children}
     </StudentContext.Provider>
-  )
-}
+  );
+};
 
-export default StudentProvider
+export default StudentProvider;
